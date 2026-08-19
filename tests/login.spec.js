5e50088cbd9f599218dcf3c68508009581b4a69e
rahafashqar <rahafashqar22@gmail.com>
2026-08-19 17:34:42 +0300

    Add login validation test

---
+++ tests/login.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Login Page Tests', () => {
+
+  test('Successful Login', async ({ page }) => {
+    await page.goto('');
+    await page.locator('#email').fill('demo@nightvault.io');
+    await page.locator('#password').fill('Passw0rd1');
+    await page.getByRole('button', { name: 'Unlock Vault' }).click();
+
+    await expect(page.getByText(/successfully logged in/i)).toBeVisible();
+  });
+
+  test('Login Validation', async ({ page }) => {
+  await page.goto('');
+  await page.getByRole('button', { name: 'Unlock Vault' }).click();
+
+  await expect(page.getByText(/email.*required/i)).toBeVisible();
+  await expect(page.getByText(/password.*required/i)).toBeVisible();
+});
+
+});
